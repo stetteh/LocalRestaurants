34fb37d274cef79cf56860bc7cc57eb986843079
stetteh <stettehq@gmail.com>
2016-05-03 10:40:26 -0500

    adding migrations

--- LocalEats/Models/Menu.cs
+++ LocalEats/Models/Menu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -5,12 +6,21 @@ namespace LocalEats.Models
 {
     public class Menu
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Restaurant Restaurant { get; set; }
         public virtual ICollection<Food> Foods { get; set; } = new List<Food>();
     }
 
     public class Food
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Double  Price { get; set; }
+        public MenuType Type { get; set; }
+        public Menu ParentMenu { get; set; }
     }
 
     public enum MenuType
--- LocalEats/Models/Restaurants.cs
+++ LocalEats/Models/Restaurants.cs
@@ -6,28 +6,38 @@ using System.Web;
 
 namespace LocalEats.Models
 {
-    public class Restaurants
+    public class Restaurant
     {
         public int Id { get; set; }
         public  string Name { get; set; }
-        public string location { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zipcode { get; set; }
         public DateTime Hours { get; set; }
         public int PhoneNumber { get; set; }
         public string Photos { get; set; }
+        public Category Category { get; set; }
         
-        public virtual ICollection<Menu> Menus { get; set; }= new List<Menu>();
+        public virtual ICollection<Menu> Menus { get; set; } = new List<Menu>();
         public virtual ICollection<Drink> Drinks { get; set; } = new List<Drink>();
         public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
     }
 
-   
-
     public class Drink
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public DrinkType Type { get; set; }
     }
 
     public class Review
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Score { get; set; }
+        public ApplicationUser User { get; set; }
     }
 
     public enum Category
@@ -37,7 +47,15 @@ namespace LocalEats.Models
         Asian,
         Mediterranean,
         Vegeterain,
-        Southern,
-        
+        Southern,  
+    }
+
+    public enum DrinkType
+    {
+        Alcohol = 1,
+        NonAlcohol,
+        Juice,
+        HotDrinks,
+        SoftDrinks,      
     }
 }
