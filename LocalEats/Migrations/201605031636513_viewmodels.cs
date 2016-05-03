d36e3910fa78d5fce116eb2fa50d612f3a4943b6
stetteh <stettehq@gmail.com>
2016-05-03 11:38:39 -0500

    additional migration

---
+++ LocalEats/Migrations/201605031636513_viewmodels.cs
@@ -0,0 +1,16 @@
+namespace LocalEats.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class viewmodels : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
