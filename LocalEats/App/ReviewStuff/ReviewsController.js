da0f4066a0bb7da01b24e183da15693a144fe45a
stetteh <stettehq@gmail.com>
2016-05-12 12:51:38 -0500

    added angualrjs factory, controller and module

---
+++ LocalEats/App/ReviewStuff/ReviewsController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ReviewController', ReviewsController);
+
+    ReviewsController.$inject = ['$scope', 'Reviewfactory'];
+
+    function ReviewsController($scope) {
+        /* jshint validthis:true */
+        //var vm = this;
+        $scope.title = 'ReviewController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ LocalEats/App/ReviewStuff/factory.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('localeatsapp')
+        .factory('Reviewfactory', factory);
+
+    factory.$inject = ['$http'];
+
+    function factory($http) {
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() { }
+    }
+})();
